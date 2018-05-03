4f66e77654c37162a7849c1062c88ea8e75c5339
Kyle Hotchkiss <kyle@kylehotchkiss.com>
2018-05-02 22:56:47 -0700

    Require password after screensaver

--- _new mac install/i-got-a-new-mac.sh
+++ _new mac install/i-got-a-new-mac.sh
@@ -5,6 +5,10 @@ sudo fdesetup enable
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
 
+# Lock screen immediately with password
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -22,3 +26,4 @@ brew cask install 1password alfred google-chrome google-drive-file-stream sequel
 
 # Install NVM (Requires editing .bashrc)
 # curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
